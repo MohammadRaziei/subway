849d2fbe15b055e17e76afe9a585657471ad58b8
Farzam <takfarzam@gmail.com>
2020-03-03 04:04:58 +0330

    added d3 visualization tools

--- MemberFiles/Farzam/Data Analysis/public/scripts/app.js
+++ MemberFiles/Farzam/Data Analysis/public/scripts/app.js
@@ -10,6 +10,67 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+// helping functions
+function TimeFormatter(stringTime) {
+  var newTime = stringTime.split(':');
+  var hour = newTime[0];
+  var minute = newTime[1];
+  return [hour, minute];
+}
+function xAxis() {
+  var x = [];
+  for (var index = 0; index < 24; index++) {
+    x = x.concat(index);
+  }
+  return x;
+}
+function dataFormatter(height, width, trainTimes) {
+  var y = new Array(24).fill(0);
+  // console.log('dataFormatter');
+  // console.log(height+1);
+  // console.log(width+1);
+  // console.log(trainTimes+1);
+  var x = xAxis(),
+      Width = parseInt(width),
+      Height = parseInt(height);
+  for (var i = 1; i < 24; i++) {
+    for (var index in trainTimes) {
+      if (TimeFormatter(trainTimes[index])[0] == i) {
+        y[i] = y[i] + 1;
+      }
+    }
+  }
+  var yMax = Math.max.apply(Math, _toConsumableArray(y)),
+      xMax = Math.max.apply(Math, _toConsumableArray(x));
+  var dt = [],
+      y0 = 0,
+      x1 = 0;
+  for (var _i = 0; _i < 24; _i++) {
+    y0 = y[_i] * Height / yMax;
+    x1 = x[_i] * Width / xMax;
+    // dt=dt.concat({x:x1,y:Height-y0});//ORIGINAL
+    dt = dt.concat({ x: x1, y: y0 });
+  }
+  return dt;
+}
+function arange(n) {
+  return [].concat(_toConsumableArray(Array(n).keys()));
+}
+function serieInator(p1, p2, p3, p4, n) {
+  var res = [];
+  for (var i = 0; i < n; i++) {
+    var a1 = p1[i],
+        a2 = p2[i],
+        a3 = p3[i],
+        a4 = p4[i],
+        res0 = {};
+    res = res.concat({ x: a1['x'], regularDay: a1['y'], thursday: a2['y'], friday: a3['y'], holiDay: a4['y'] });
+  }
+  return res;
+}
+
 var VisibilityToggle = function (_React$Component) {
   _inherits(VisibilityToggle, _React$Component);
 
@@ -18,21 +79,28 @@ var VisibilityToggle = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
 
+    _this.onFormSubmit = _this.onFormSubmit.bind(_this);
     _this.changeDay = _this.changeDay.bind(_this);
     _this.changeLine = _this.changeLine.bind(_this);
     _this.clickRoute = _this.clickRoute.bind(_this);
     _this.changeRoute = _this.changeRoute.bind(_this);
     _this.clickStation = _this.clickStation.bind(_this);
     _this.changekStation = _this.changekStation.bind(_this);
-    _this.fetchLine = _this.fetchLine.bind(_this);
-    _this.state = { RouteVis: false, StVis: false, day: '', sign: '',
+    _this.fetchStatioData = _this.fetchStatioData.bind(_this);
+    _this.state = { RouteVis: false, StVis: false, day: '', btnVis: false, sign: '',
       line: '', station: '', linecolor: ['#ffffff', '#ee2e24', '#0b55a0', '#2bbdee', '#fcc204'] };
     return _this;
   }
+  // data = Object.assign(d3.csvParse(await FileAttachment("unemployment-2.csv").text(), d3.autoType), {y: "Unemployment"})
   //event handlers
 
 
   _createClass(VisibilityToggle, [{
+    key: 'onFormSubmit',
+    value: function onFormSubmit(e) {
+      e.preventDefault();
+    }
+  }, {
     key: 'changeDay',
     value: function changeDay() {
       var newDay = document.getElementById('DL').value;
@@ -134,40 +202,136 @@ var VisibilityToggle = function (_React$Component) {
       this.setState(function (prevState) {
         return { station: { 'index': val, 'name': stName } };
       });
-    }
-  }, {
-    key: 'fetchLine',
-    value: function fetchLine() {
-      var day = this.state.day;
-      var sign = this.state.sign;
-      var station = this.state.station;
-      var line = this.state.line;
-      // fetch(`../src/trainData/trainData_line0${line[4]}_${sign}_${day}.csv`).then(response => response.text()).then(function (data) {localStorage.setItem('stationsData',data);});
-      d3.csv('../src/trainData/trainData_line0' + line[4] + '_' + sign + '_' + day + '.csv', function (data) {
-        console.log(data);
-        console.log(station);
-        console.log(station['index']);
-        console.log(data[station['index']]);
+      this.setState(function (prevState) {
+        return { btnVis: true };
       });
     }
-    // helping functions
+  }, {
+    key: 'fetchStatioData',
+    value: function fetchStatioData() {
+      localStorage.setItem('index', 0);
+      var height = 400,
+          width = 600;
+      var sign = this.state.sign,
+          station = this.state.station,
+          line = this.state.line;
+      // console.log(`${line} of route${sign} at station${station} on ${day}`);const day=this.state.day;
 
-  }, {
-    key: 'TimeFormatter',
-    value: function TimeFormatter(stringTime) {
-      var newTime = stringTime.split(':');
-      var hour = newTime[0];
-      var minute = newTime[1];
-      return [hour, minute];
-    }
-  }, {
-    key: 'xAxis',
-    value: function xAxis() {
-      var x = [];
-      for (var index = 0; index < 24; index++) {
-        x = x.concat(index);
-      }
-      return x;
+      /*
+        d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_${day}.csv`,
+          function (data) {
+            const i=parseInt(localStorage.getItem('index'));
+            if (i==station['index']) {
+              let ds=dataFormatter(height,width,data); // create svg element:
+              // console.log(ds);
+              let svg = d3.select("svg").attr("width", width).attr("height", height);
+              let curveFunc = d3.area()
+                .x(function(d) { return d.x }) // Position of both line breaks on the X axis
+                .y1(function(d) { return d.y }) // Y position of top line breaks
+                .y0(height);  // Y position of bottom line breaks (200 = bottom of svg area)
+              // Add the path using this helper function 
+              svg.append('path')
+               .attr('d', curveFunc(ds))
+               .attr('stroke', 'black')
+               .attr('fill', '#69b3a2');  
+            }
+            localStorage.setItem('index',i+1);
+          }
+        );
+      */
+
+      localStorage.setItem('index1', 0);
+      d3.csv('../src/trainData/trainData_line0' + line[4] + '_' + sign + '_regularDay.csv', function (data) {
+        var i = parseInt(localStorage.getItem('index1'));
+        if (i == station['index']) {
+          var ds = dataFormatter(height, width, data); // create svg element:
+          localStorage.setItem('regD', JSON.stringify(ds));
+        }
+        localStorage.setItem('index1', i + 1);
+      });
+      localStorage.setItem('index2', 0);
+      d3.csv('../src/trainData/trainData_line0' + line[4] + '_' + sign + '_thursday.csv', function (data) {
+        var i = parseInt(localStorage.getItem('index2'));
+        if (i == station['index']) {
+          var ds = dataFormatter(height, width, data); // create svg element:
+          localStorage.setItem('thursD', JSON.stringify(ds));
+        }
+        localStorage.setItem('index2', i + 1);
+      });
+      localStorage.setItem('index3', 0);
+      d3.csv('../src/trainData/trainData_line0' + line[4] + '_' + sign + '_friday.csv', function (data) {
+        var i = parseInt(localStorage.getItem('index3'));
+        if (i == station['index']) {
+          var ds = dataFormatter(height, width, data); // create svg element:
+          localStorage.setItem('friD', JSON.stringify(ds));
+        }
+        localStorage.setItem('index3', i + 1);
+      });
+      localStorage.setItem('index4', 0);
+      d3.csv('../src/trainData/trainData_line0' + line[4] + '_' + sign + '_holiDay.csv', function (data) {
+        var i = parseInt(localStorage.getItem('index4'));
+        if (i == station['index']) {
+          var ds = dataFormatter(height, width, data); // create svg element:
+          localStorage.setItem('holiD', JSON.stringify(ds));
+        }
+        localStorage.setItem('index4', i + 1);
+      });
+      var ds0 = JSON.parse(localStorage.getItem('regD'));
+      var ds1 = JSON.parse(localStorage.getItem('thursD'));
+      var ds2 = JSON.parse(localStorage.getItem('friD'));
+      var ds3 = JSON.parse(localStorage.getItem('holiD'));
+      console.log('inator:');
+      var data = serieInator(ds0, ds1, ds2, ds3, ds0['length']);
+      console.log(data);
+      var svg = d3.select("svg").attr("width", width).attr("height", height);
+      // const color = d3.scaleOrdinal()
+      //     .domain(data.columns.slice(1))
+      //     .range(d3.schemeCategory10);s
+      var z = d3.interpolateCool;
+
+      var stack = d3.stack().keys(['regularDay', 'thursday', 'friday', 'holiDay']).order(d3.stackOrderNone).offset(d3.stackOffsetNone);
+
+      var series = stack(data);
+      localStorage.setItem('data', JSON.stringify(data));
+      localStorage.setItem('series', JSON.stringify(series));
+      // series = d3.stack()
+      //     .keys(data.columns.slice(1))
+      //     .offset(d3.stackOffsetWiggle)
+      //     .order(d3.stackOrderInsideOut)
+      //   (data);
+      var margin = { top: 0, right: 20, bottom: 30, left: 20 };
+      var x = d3.scaleUtc().domain(d3.extent(data, function (d) {
+        return d.x;
+      })).range([margin.left, width - margin.right]);
+      var y = d3.scaleLinear().domain([d3.min(series, function (d) {
+        return d3.min(d, function (d) {
+          return d[0];
+        });
+      }), d3.max(series, function (d) {
+        return d3.max(d, function (d) {
+          return d[1];
+        });
+      })]).range([height - margin.bottom, margin.top]);
+
+      var area = d3.area().x(function (d) {
+        return x(d.data.x);
+      }).y0(function (d) {
+        return y(d[0]);
+      }).y1(function (d) {
+        return y(d[1]);
+      });
+
+      svg.append("g").selectAll("path").data(series).join("path")
+      // .attr("fill", ({key}) => color(key))
+      .attr("d", area).attr("fill", function () {
+        return z(Math.random());
+      }).append("title").text(function (_ref) {
+        var key = _ref.key;
+        return key;
+      });
+
+      // svg.append("g")
+      //     .call(xAxis);
     }
   }, {
     key: 'render',
@@ -178,12 +342,12 @@ var VisibilityToggle = function (_React$Component) {
         React.createElement(
           'h1',
           null,
-          'Visibility Toggle'
+          'Data Visualization'
         ),
         React.createElement(
           'form',
-          null,
-          React.createElement(
+          { onSubmit: this.onFormSubmit },
+          false && React.createElement(
             'select',
             { onChange: this.changeDay, defaultValue: 'noDay', id: 'DL' },
             React.createElement(
@@ -269,13 +433,21 @@ var VisibilityToggle = function (_React$Component) {
               '--select station--'
             )
           ),
-          this.state.day && this.state.sign && this.state.line && this.state.station && React.createElement(
+          React.createElement(
             'button',
-            { onClick: this.fetchLine },
-            'fetchLine'
+            { disabled: !this.state.btnVis, onClick: this.fetchStatioData },
+            'fetchStatioData'
           )
         ),
-        React.createElement('svg', null)
+        React.createElement(
+          'div',
+          { id: 'dataSVG', className: 'area' },
+          React.createElement(
+            'svg',
+            null,
+            'svg here'
+          )
+        )
       );
     }
   }]);
--- MemberFiles/Farzam/Data Analysis/src/app.js
+++ MemberFiles/Farzam/Data Analysis/src/app.js
@@ -1,17 +1,70 @@
+// helping functions
+function TimeFormatter(stringTime) {
+  var newTime=stringTime.split(':');
+  var hour=newTime[0];
+  var minute=newTime[1];  
+  return [hour,minute];
+}
+function xAxis(){
+  let x=[];
+for (let index = 0; index < 24; index++) {
+  x=x.concat(index);
+  }
+  return x;
+}
+function dataFormatter(height,width,trainTimes){
+  let y= new Array(24).fill(0);
+  // console.log('dataFormatter');
+  // console.log(height+1);
+  // console.log(width+1);
+  // console.log(trainTimes+1);
+  const x=xAxis(),Width=parseInt(width),Height=parseInt(height);
+  for (let i = 1; i < 24; i++) {
+    for (let index in trainTimes) {
+      if (TimeFormatter(trainTimes[index])[0]==i) {
+        y[i]=y[i]+1;
+      }
+    }
+  }
+  const yMax=Math.max(...y),xMax=Math.max(...x);
+  let dt=[],y0=0,x1=0;
+  for (let i = 0; i < 24; i++) {
+    y0=y[i]*Height / yMax;
+    x1=x[i] * Width / xMax;
+    // dt=dt.concat({x:x1,y:Height-y0});//ORIGINAL
+    dt=dt.concat({x:x1,y:y0});
+} 
+  return dt;
+}
+function arange(n) {
+  return [...Array(n).keys()];
+}
+function serieInator(p1,p2,p3,p4,n) {
+  let res=[];
+  for (let i = 0; i < n; i++) {
+    let a1=p1[i],a2=p2[i],a3=p3[i],a4=p4[i],res0={};
+    res=res.concat(     {x:a1['x'],regularDay:a1['y'],thursday:a2['y'],friday:a3['y'],holiDay:a4['y']}    );
+  }
+  return(res);
+}
+
 class VisibilityToggle extends React.Component {
   constructor(props) {
     super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.changeDay = this.changeDay.bind(this);
     this.changeLine = this.changeLine.bind(this);
     this.clickRoute = this.clickRoute.bind(this);
     this.changeRoute = this.changeRoute.bind(this);
     this.clickStation = this.clickStation.bind(this);
     this.changekStation = this.changekStation.bind(this);
-    this.fetchLine = this.fetchLine.bind(this);
-    this.state = {RouteVis: false,StVis:false,day:'',sign:'',
+    this.fetchStatioData = this.fetchStatioData.bind(this);
+    this.state = {RouteVis: false,StVis:false,day:'',btnVis:false,sign:'',
     line:'',station:'',linecolor:['#ffffff','#ee2e24','#0b55a0','#2bbdee','#fcc204']};
   }
+  // data = Object.assign(d3.csvParse(await FileAttachment("unemployment-2.csv").text(), d3.autoType), {y: "Unemployment"})
 //event handlers
+onFormSubmit(e){e.preventDefault();}
 changeDay(){
   let newDay=document.getElementById('DL').value;
   this.setState(   (prevState) => { return{day: newDay}; }     );
@@ -80,46 +133,146 @@ changekStation(){
   const val=document.getElementById('SL').value;
   const stName=document.getElementById(val).text;
   this.setState((prevState)=>{  return {station:{'index':val,'name':stName}};   }     );
+  this.setState((prevState)=>{  return {btnVis:true   }}     );
 }
-fetchLine(){
-  const day=this.state.day;
-  const sign=this.state.sign;
-  const station=this.state.station;
-  const line=this.state.line;
-  // fetch(`../src/trainData/trainData_line0${line[4]}_${sign}_${day}.csv`).then(response => response.text()).then(function (data) {localStorage.setItem('stationsData',data);});
-  d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_${day}.csv`,function (data) {
-    console.log(data);
-    console.log(station);
-    console.log(station['index']);
-    console.log(data[station['index']]);
-    });
-}
-// helping functions
-TimeFormatter(stringTime) {
-  var newTime=stringTime.split(':');
-  var hour=newTime[0];
-  var minute=newTime[1];  
-  return [hour,minute];
-}
-xAxis(){
-  let x=[];
-for (let index = 0; index < 24; index++) {
-  x=x.concat(index);
+fetchStatioData(){
+  localStorage.setItem('index',0);
+  const height=400,width=600;
+  const  sign=this.state.sign, station=this.state.station, line=this.state.line;
+  // console.log(`${line} of route${sign} at station${station} on ${day}`);const day=this.state.day;
+  
+  /*
+    d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_${day}.csv`,
+      function (data) {
+        const i=parseInt(localStorage.getItem('index'));
+        if (i==station['index']) {
+          let ds=dataFormatter(height,width,data); // create svg element:
+          // console.log(ds);
+          let svg = d3.select("svg").attr("width", width).attr("height", height);
+          let curveFunc = d3.area()
+            .x(function(d) { return d.x }) // Position of both line breaks on the X axis
+            .y1(function(d) { return d.y }) // Y position of top line breaks
+            .y0(height);  // Y position of bottom line breaks (200 = bottom of svg area)
+          // Add the path using this helper function 
+          svg.append('path')
+           .attr('d', curveFunc(ds))
+           .attr('stroke', 'black')
+           .attr('fill', '#69b3a2');  
+        }
+        localStorage.setItem('index',i+1);
+      }
+    );
+*/
+
+  localStorage.setItem('index1',0);
+  d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_regularDay.csv`,
+    function (data) {
+      const i=parseInt(localStorage.getItem('index1'));
+      if (i==station['index']) {
+        let ds=dataFormatter(height,width,data); // create svg element:
+        localStorage.setItem('regD',JSON.stringify(ds));
+      }
+      localStorage.setItem('index1',i+1);
+    }
+  );
+  localStorage.setItem('index2',0);
+  d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_thursday.csv`,
+    function (data) {
+      const i=parseInt(localStorage.getItem('index2'));
+      if (i==station['index']) {
+        let ds=dataFormatter(height,width,data); // create svg element:
+        localStorage.setItem('thursD',JSON.stringify(ds));
+      }
+      localStorage.setItem('index2',i+1);
+    }
+  );
+  localStorage.setItem('index3',0);
+  d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_friday.csv`,
+    function (data) {
+    const i=parseInt(localStorage.getItem('index3'));
+    if (i==station['index']) {
+      let ds=dataFormatter(height,width,data); // create svg element:
+      localStorage.setItem('friD',JSON.stringify(ds));
+    }
+    localStorage.setItem('index3',i+1);
+    }
+  );
+  localStorage.setItem('index4',0);
+  d3.csv(`../src/trainData/trainData_line0${line[4]}_${sign}_holiDay.csv`,
+    function (data) {
+  const i=parseInt(localStorage.getItem('index4'));
+  if (i==station['index']) {
+    let ds=dataFormatter(height,width,data); // create svg element:
+    localStorage.setItem('holiD',JSON.stringify(ds));
   }
-  return x;
+  localStorage.setItem('index4',i+1);
+    }
+  );
+let ds0=JSON.parse(localStorage.getItem('regD'));
+let ds1=JSON.parse(localStorage.getItem('thursD'));
+let ds2=JSON.parse(localStorage.getItem('friD'));
+let ds3=JSON.parse(localStorage.getItem('holiD'));
+console.log('inator:');
+const data=serieInator(ds0,ds1,ds2,ds3,ds0['length']);
+console.log(data);
+let svg = d3.select("svg").attr("width", width).attr("height", height);
+// const color = d3.scaleOrdinal()
+//     .domain(data.columns.slice(1))
+//     .range(d3.schemeCategory10);s
+var z = d3.interpolateCool
+
+var stack = d3.stack()
+.keys(['regularDay','thursday','friday','holiDay'])
+.order(d3.stackOrderNone)
+.offset(d3.stackOffsetNone);
+
+var series = stack(data);
+localStorage.setItem('data',JSON.stringify(data));
+localStorage.setItem('series',JSON.stringify(series));
+// series = d3.stack()
+//     .keys(data.columns.slice(1))
+//     .offset(d3.stackOffsetWiggle)
+//     .order(d3.stackOrderInsideOut)
+//   (data);
+const margin = ({top: 0, right: 20, bottom: 30, left: 20});
+let x = d3.scaleUtc()
+    .domain(d3.extent(data, d => d.x))
+    .range([margin.left, width - margin.right]);
+let y = d3.scaleLinear()
+    .domain([d3.min(series, d => d3.min(d, d => d[0])), d3.max(series, d => d3.max(d, d => d[1]))])
+    .range([height - margin.bottom, margin.top]);
+
+const area = d3.area()
+    .x(d => x(d.data.x))
+    .y0(d => y(d[0]))
+    .y1(d => y(d[1]));
+
+svg.append("g")
+    .selectAll("path")
+    .data(series)
+    .join("path")
+        // .attr("fill", ({key}) => color(key))
+        .attr("d", area)
+        .attr("fill", () => z(Math.random()))
+    .append("title")
+        .text(({key}) => key);
+
+// svg.append("g")
+//     .call(xAxis);
 }
+
   
   render() {
     return (<div>
-      <h1>Visibility Toggle</h1>
-      <form>
-        <select onChange={this.changeDay} defaultValue='noDay' id='DL'>
+      <h1>Data Visualization</h1>
+      <form onSubmit={this.onFormSubmit}>
+        {false && (<select onChange={this.changeDay} defaultValue='noDay' id='DL'>
           <option className='day' id='nd' disabled='true' value="noDay">--select day--</option>
           <option className='day' id="day1" value="regularDay">Regural Day</option>
           <option className='day' id="day2" value="thursday">Thursday</option>
           <option className='day' id="day3" value="friday">Friday</option>
           <option className='day' id="day4" value="holiDay">Holliday</option>
-        </select>
+        </select>)}
         {true &&(<select className='line' onChange={this.changeLine} defaultValue='noLine' id='LL'>
           <option className='line' id='nl' disabled='true' value="noLine">--select line--</option>
           <option className='line' id="line1" value="line1">line 1</option>
@@ -137,10 +290,10 @@ for (let index = 0; index < 24; index++) {
         {this.state.RouteVis&&  (<select id='SL' disabled={!this.state.StVis} defaultValue='noStation' onClick={this.clickStation} onChange={this.changekStation}>    
           <option disabled='true' value='noStation'>--select station--</option>
         </select>)}
-      
-        {this.state.day && this.state.sign && this.state.line && this.state.station && (<button onClick={this.fetchLine}>fetchLine</button>)}
-      </form>
-      <svg></svg>
+        {/*<button disabled={!(this.state.day && this.state.btnVis)} onClick={this.fetchStatioData}>fetchStatioData</button>*/}
+        <button disabled={!(this.state.btnVis)} onClick={this.fetchStatioData}>fetchStatioData</button>
+        </form>
+      <div id='dataSVG' className='area'><svg>svg here</svg></div>
     </div>);
   }
 }
